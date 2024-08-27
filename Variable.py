f004e917e660bd83d64b5f6537d2bd0295653725
BelichenkoArtem <145715207+BelichenkoArtem@users.noreply.github.com>
2024-08-27 11:54:07 +0300

    Add files via upload

---
+++ Variable.py
@@ -0,0 +1,5 @@
+done_hw = 12
+hours = 1.5
+nameCourse = 'Python'
+task_per_time = done_hw/hours
+print('Курс:', nameCourse,', всего задач:',done_hw,', затрачено часов: ', hours,', среднее время выполнения', task_per_time, 'часа.')
