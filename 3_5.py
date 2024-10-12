6fce093082953119ce6fd750c22853e61563b66a
BelichenkoArtem <145715207+BelichenkoArtem@users.noreply.github.com>
2024-10-12 22:42:58 +0300

    Add files via upload

--- 3_5.py
+++ 3_5.py
@@ -1,14 +1,14 @@
 def get_multiplied_digits(number):
-    #print(type(number))
     str_number = str(number)
+    while str_number and str_number[-1] == '0':
+        str_number = str_number[:-1]
     list_number = list(str_number)
     first = list_number[0]
-    if len(list_number) == 0:
-        return 0
-    elif len(list_number) == 1:
+    if len(list_number) == 1:
         return int(first)
     else:
         return int(first) * get_multiplied_digits(int(str_number[1:]))
 
-result = get_multiplied_digits(40203)
+
+result = get_multiplied_digits(4020)
 print(result)
