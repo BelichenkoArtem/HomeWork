30ecc20a6e83a27d0910b21e08433e253f35a7b4
BelichenkoArtem <145715207+BelichenkoArtem@users.noreply.github.com>
2025-01-16 12:25:50 +0300

    Add files via upload

--- module 9/9_1.py
+++ module 9/9_1.py
@@ -1,27 +1,20 @@
-def apply_to_list(data, func):
-    try:
-        return func(data)
-    except TypeError:
-        return None
-    
-def apply_to_each(data, func):
-    try:
-        return list(map(func, data))
-    except TypeError:
-        return None
-
 def apply_all_func(int_list, *functions):
     results = {}
     for function in functions:
         try:
-            result = apply_to_list(int_list, function)
-            if result is None:
-                result = apply_to_each(int_list, function)
+            result = function(int_list)  # Применяем функцию напрямую к списку
         except Exception as e:
-            result = f'Ошибка при применении {function.__name__}: {e}'
-        results[function.__name__ ] = result
+            result = f'Ошибка при применении {function.__name__}: {e}' # Обработка ошибок
+        results[function.__name__] = result
     return results
 
-
+# Примеры использования:
 print(apply_all_func([6, 20, 15, 9], max, min))
 print(apply_all_func([6, 20, 15, 9], len, sum, sorted))
+print(apply_all_func([1, 2, 'a'], sum))  # Пример с некорректным типом в списке
+print(apply_all_func([], sum)) # Пример с пустым списком
+
+def my_func(data): # Пример пользовательской функции
+    return [x+1 for x in data]
+
+print(apply_all_func([1,2,3], my_func))
